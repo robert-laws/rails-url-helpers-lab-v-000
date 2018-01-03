f1420e56dab81c6f53ca124528325fddda1ca2b7
Robert Laws <roblaws@me.com>
2018-01-03 13:29:06 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :students, only: [:index, :show]
 
+  resources :activate_student, only: :show
+
   # This show route can be refactored into the above resources method call, like so:
   # resources :students, only: [:index, :show]
   # However, for the sake of this lab and seeing how you can pass params through
